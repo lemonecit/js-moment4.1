18008938808cef9df168f22b4d0d0c5d677c0a5b
lemonec <pierre.lemonec@gmail.com>
2021-10-02 19:02:07 +0300

    final 4.1

--- js/main.js
+++ js/main.js
@@ -2,38 +2,33 @@
 I denna fil lägger du din JavaScript-kod för att lösa uppgiften.
 */
 
-const movies = {
-    getinformation: function(title,category,playtime) {
-      return this.title + this.category + this.playtime;
-    }
-  }
+// Liberal notation vibraler
+const movie1 = {title: "star wars", category: "Science Fiction", playtime:190 };
+const movie2 = {title: "Introduction to Javascript", category: "Utbildning", playtime:15 };
+const movie3 = {title: "The last fotsteps of a viking in Birka", category: "Documentary", playtime:190 };
+const movie4 = {title: "Farlig kärlekförbindelse", category: "Action", playtime:190 };
 
-  const movie1 = {
-    title: "Star war",
-    category: "Science Fiction",
-    playtime: "104"
-  }
+// function accessing from liberal notation vibraler
+let movies = {
+      getInformation:function() {
+          return     movie1.title + " " +  movie1.category + " " + movie1.playtime + "\n" +
+          movie2.title + " " +  movie2.category + " " + movie2.playtime + "\n" +
+          movie3.title + " " +  movie3.category + " " + movie3.playtime + "\n" + 
+          movie4.title + " " +  movie4.category + " " + movie4.playtime + "\n";
 
-  const movie2 = {
-    title: "Star war1",
-    category: "Science Fiction",
-    playtime: "104"
-  }
+        }
+      }
 
-  const movie3 = {
-    title: "Star war2",
-    category: "Science Fiction",
-    playtime: "104"
-  }
-
-  const movie4 = {
-    title: "Star war3",
-    category: "Science Fiction",
-    playtime: "104"
-  }
+      // utskrift
+let utskrift = document.getElementById("output").innerHTML += movies.getInformation();
 
 
-let info = document.getElementById("output").innerHTML = movie4.category;
+
+
+
+
+
+
 
 
 
